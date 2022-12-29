8a857d14c93f0e13fa9c92cb592910b6458304a1
M. Abdullah Butt <81871162+AbdullahButt2611@users.noreply.github.com>
2022-12-29 14:12:36 +0500

    Nav Bar Responsiveness
    The responsiveness of the working navbar have been completed on the medium screens.

---
+++ main.js
@@ -0,0 +1,29 @@
+const navItems = document.querySelector('.nav__items');
+const openNavBtn = document.querySelector('#open__nav-btn');
+const closeNavBtn = document.querySelector('#close__nav-btn');
+
+
+
+
+// Open Nav Dropdown
+const openNav = () => {
+    navItems.style.display = 'flex';
+    openNavBtn.style.display = 'none';
+    closeNavBtn.style.display = 'inline-block';
+}
+
+
+// Close Nav Dropdown
+const closeNav = () => {
+    navItems.style.display = 'none';
+    openNavBtn.style.display = 'inline-block';
+    closeNavBtn.style.display = 'none';
+}
+
+
+
+
+
+// =========================        Calling Functions   ===============================
+openNavBtn.addEventListener("click", openNav);
+closeNavBtn.addEventListener("click", closeNav);
